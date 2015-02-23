e68a57cabf3b08b8ad5050de56d0a0a7f507f9f4
DogecoinDark <justinvalo@yahoo.com>
2015-02-23 08:07:30 -0500

    Update install.bat
    new version of i2p

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@ mkdir %userprofile%\AppData\Roaming\DogecoinDark\
 mkdir %userprofile%\AppData\Roaming\i2p\
 copy i2ptunnel.config %userprofile%\AppData\Roaming\i2p\
 copy DogeCoinDark.conf %userprofile%\AppData\Roaming\DogecoinDark\
-i2pinstall_0.9.17_windows.exe
+i2pinstall_0.9.18_windows.exe
 @echo off
 echo press a key to continue i2p/doged wallet installation. this next part will start i2p, (since you have just installed it) and then launch the DogecoinDark wallet on the i2p network. please allow a few minutes for i2p to establish it's first connection. within a few moments the wallet should start making connections. 
 pause
